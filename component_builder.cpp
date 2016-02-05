8d2dd57e42fc2a6f24c92c83e9739a78ba2921ae
5cript <Tim06TR@gmail.com>
2016-02-05 17:33:42 +0100

    Started class for ui element generation from markup.

--- component_builder.cpp
+++ component_builder.cpp
@@ -1,6 +1,45 @@
 #include "component_builder.h"
 
-ComponentBuilder::ComponentBuilder()
+#include "wiki-markup/parser/page_parser.hpp"
+
+#include <QDebug>
+
+//####################################################################################
+PageBuilder::PageBuilder()
 {
 
 }
+//-----------------------------------------------------------------------------------
+void PageBuilder::loadMarkup(std::string const& markup)
+{
+    using namespace WikiMarkup;
+    using namespace Components;
+
+    PageParser parser (markup);
+    parser.parse();
+
+    page_ = parser.getPage();
+}
+//-----------------------------------------------------------------------------------
+std::string PageBuilder::toMarkup() const
+{
+    return page_.toMarkup();
+}
+//-----------------------------------------------------------------------------------
+void PageBuilder::generateUiElements(QLayout *parent)
+{
+    auto& components = page_.getComponents();
+
+    for (auto const& i : components)
+    {
+        addLabel(parent, i->getMetaInfo().name);
+    }
+}
+//-----------------------------------------------------------------------------------
+void PageBuilder::addLabel(QLayout *parent, std::string const& text)
+{
+    QLabel* label = new QLabel();
+    label->setText(QString::fromStdString(text));
+    parent->addWidget(label);
+}
+//####################################################################################
--- component_builder.h
+++ component_builder.h
@@ -1,12 +1,47 @@
 #ifndef COMPONENTBUILDER_H
 #define COMPONENTBUILDER_H
 
+#include "wiki-markup/page.hpp"
 
-class ComponentBuilder
+#include <QWidget>
+#include <QLabel>
+#include <QLayout>
+
+#include <string>
+
+class PageBuilder
 {
 public:
-    ComponentBuilder();
-    ~ComponentBuilder() = default;
+    /**
+     * Loads markup from a string and generates a page.
+     * Does not automatically generate ui elements.
+     *
+     * @param markup Some markup data.
+     */
+    void loadMarkup(std::string const& markup);
+
+    /**
+     * Converts the page into markup.
+     *
+     * @return Wiki markup.
+     */
+    std::string toMarkup() const;
+
+    /**
+     * Generates UiElements from a wiki page.
+     *
+     * @param parent A layout that shall contain all the controls.
+     */
+    void generateUiElements(QLayout* parent);
+
+    PageBuilder();
+    ~PageBuilder() = default;
+
+private:
+    void addLabel(QLayout* parent, std::string const& text);
+
+private:
+    WikiMarkup::Page page_;
 };
 
 #endif // COMPONENTBUILDER_H
