a4f0d8c0434222b2958a580d4f21add5f4f973f7
5cript <Tim06TR@gmail.com>
2016-02-05 17:34:07 +0100

    Added menu, exit menu action and load menu action.

--- editor.cpp
+++ editor.cpp
@@ -1,14 +1,47 @@
 #include "editor.h"
 #include "ui_editor.h"
 
+#include <QDebug>
+#include <QMessageBox>
+#include <QFileDialog>
+
+#include <fstream>
+
+//####################################################################################
 Editor::Editor(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Editor)
 {
     ui->setupUi(this);
 }
-
+//-----------------------------------------------------------------------------------
 Editor::~Editor()
 {
     delete ui;
 }
+//-----------------------------------------------------------------------------------
+void Editor::on_action_LOAD_triggered()
+{
+    qDebug() << "Hello";
+
+    auto fileName = QFileDialog::getOpenFileName(this, "Open a markup file", ".", "Text (*.mu *.txt)");
+
+    std::ifstream reader (fileName.toStdString(), std::ios_base::binary);
+
+    if (!reader.good()) {
+        QMessageBox::warning(this, "IO Error", "Could not load the file specified", QMessageBox::Ok, QMessageBox::Ok);
+        return;
+    }
+
+    std::string data;
+    do {
+        char buffer[4096];
+        reader.read(buffer, 4096);
+        data.append(buffer, reader.gcount());
+    } while (reader.gcount() == 4096);
+
+    qDebug() << QString::fromStdString(data);
+    builder_.loadMarkup(data);
+    builder_.generateUiElements(ui->viewPort->layout());
+}
+//####################################################################################
--- editor.h
+++ editor.h
@@ -1,6 +1,8 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+#include "component_builder.h"
+
 #include <QMainWindow>
 
 namespace Ui {
@@ -15,7 +17,11 @@ public:
     explicit Editor(QWidget *parent = 0);
     ~Editor();
 
+private slots:
+    void on_action_LOAD_triggered();
+
 private:
+    PageBuilder builder_;
     Ui::Editor *ui;
 };
 
