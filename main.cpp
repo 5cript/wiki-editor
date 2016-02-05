8b3f8fab6da3cf72d8a651c793f27f1b2c695acd
5cript <Tim06TR@gmail.com>
2016-02-05 17:34:25 +0100

    Ui is now loading style.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "editor.h"
+
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // extract style sheet
+    if (!QFile("./stylesheet.qss").exists())
+        QFile::copy(":/styles/default.qss", "./stylesheet.qss");
+
+    QFile File("stylesheet.qss");
+    File.open(QFile::ReadOnly);
+    QString StyleSheet = QLatin1String(File.readAll());
+    a.setStyleSheet(StyleSheet);
+
+    // show main window and run app
     Editor w;
     w.show();
 
